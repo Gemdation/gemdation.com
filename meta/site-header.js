4e22ed9abc3fc8967620b6505450666a3cc79557
Gemdation <gem@techie.com>
2026-02-19 15:45:19 -0600

    Added old writing and fixed some stuff

--- meta/site-header.js
+++ meta/site-header.js
@@ -1,14 +1,14 @@
 const headerData = {
   path: '/meta/',
   files: [
-    'header-3dumb.png', 'header-aftershok.png', 'header-asset.png', 'header-barcode.png',
+    'header-2dumb.png','header-3dumb.png', 'header-aftershok.png', 'header-akronim.png', 'header-anagram.png', 'header-asset.png', 'header-barcode.png',
     'header-bloody.png', 'header-borel.png', 'header-bpdots.png', 'header-braille.png',
-    'header-cherrycreamsoda.png', 'header-comicsans.png', 'header-dekko.png', 'header-delicious.png',
-    'header-droidsans.png', 'header-missfajardose.png', 'header-overload.png', 'header-plasmadrip.png',
+    'header-cherrycreamsoda.png', 'header-comicsans.png', 'header-crackman.png', 'header-dekko.png', 'header-delicious.png',
+    'header-droidsans.png', 'header-google.png', 'header-missfajardose.png', 'header-mojangles.png', 'header-overload.png', 'header-plasmadrip.png',
     'header-portersans.png', 'header-pressstart.png', 'header-profontwin.png', 'header-ralewaydots.png',
     'header-rampartone.png', 'header-redacted.png', 'header-robotocondense.png', 'header-rubikbeastly.png',
-    'header-sarina.png', 'header-sceps3.png', 'header-segoe.png', 'header-sigmarone.png',
-    'header-timesnewroman.png', 'header-verdana.png', 'header-walmart.png', 'header-wingdings.png'
+    'header-sarina.png', 'header-sceps3.png', 'header-scribble.png', 'header-segoe.png', 'header-sigmarone.png',
+    'header-timesnewroman.png', 'header-verdana.png', 'header-walmart.png', 'header-wingdings.png', 'header-yahoo.png'
   ]
 };
 
